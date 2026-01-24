6103170280f42a0eee912987ceea258eb8b24933
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2026-01-24 14:32:11 +0000

    Update components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,16 @@
 import { Outlet } from '@tanstack/react-router'
-import { HelmetProvider } from 'react-helmet-async'
 
 import Header from './Header'
 import Footer from './Footer'
 
 export default function Layout() {
   return (
-    <HelmetProvider>
-      <div className="flex min-h-screen flex-col">
-        <Header />
-        <main className="flex-1">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </HelmetProvider>
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Helmet } from 'react-helmet-async'
+import { Helmet } from 'react-helmet'
 
 import Hero from '../components/Hero'
 import Features from '../components/Features'
